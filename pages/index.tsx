f5fbe85bd917eb60e862a4e6c1f55a4784055a7c
Jason Wiker <jason.wiker@gmail.com>
2021-12-28 17:21:32 -0700

    Remove imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,37 +6,8 @@ import Skills from "../components/home/Skills";
 import CurrentWork from "../components/home/CurrentWork";
 import Footer from "../components/home/Footer";
 import BlogPosts from "../components/home/BlogPosts";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 const Home: NextPage = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get("https://dev.to/api/articles?username=wiker").then((res) => {
-      console.log(res.data);
-      setData(
-        res.data.map((post: any) => {
-          return {
-            title: post.title,
-            href: post.url,
-            category: { name: post.type_of, href: "#" },
-            description: post.description,
-            date: post.readable_publish_date,
-            datetime: post.published_at,
-            imageUrl: post.social_image,
-            readingTime: `${post.reading_time_minutes} min`,
-            author: {
-              name: post.user.name,
-              href: post.user.website_url,
-              imageUrl: post.user.profile_image,
-            },
-          };
-        })
-      );
-    });
-  }, []);
-
   return (
     <div className="h-screen">
       <Head>
