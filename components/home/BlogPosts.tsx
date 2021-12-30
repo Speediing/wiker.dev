b7e0c45cf5e068529c064b280c0b099681249c5a
Jason Wiker <jason.wiker@gmail.com>
2021-12-30 15:49:49 -0700

    debug log

--- components/home/BlogPosts.tsx
+++ components/home/BlogPosts.tsx
@@ -7,6 +7,30 @@ export default function BlogPosts() {
 
   useEffect(() => {
     axios.get("https://dev.to/api/articles?username=wiker").then((res) => {
+      console.log(
+        res.data
+          .map((post: any) => {
+            return {
+              title: post.title,
+              href: post.canonical_url,
+              category: {
+                name: post.tag_list.map((x: string) => `#${x}`).join(" "),
+                href: "#",
+              },
+              description: post.description,
+              date: post.readable_publish_date,
+              datetime: post.published_at,
+              imageUrl: post.social_image,
+              readingTime: `${post.reading_time_minutes} min`,
+              author: {
+                name: post.user.name,
+                href: "https://dev.to/wiker",
+                imageUrl: post.user.profile_image,
+              },
+            };
+          })
+          .slice(0, 3)
+      );
       setData(
         res.data
           .map((post: any) => {
